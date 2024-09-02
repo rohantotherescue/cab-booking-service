330c5b6497d06cad01e34e84000320cb5f4cbdf4
rohantotherescue <rohantotherescue@gmail.com>
2024-09-03 03:27:25 +0530

    import error fixes

--- cab-book-user/src/main/java/com/kafkaproject/cab_book_user/service/LocationService.java
+++ cab-book-user/src/main/java/com/kafkaproject/cab_book_user/service/LocationService.java
@@ -1,6 +1,7 @@
 package com.kafkaproject.cab_book_user.service;
 
-
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 @Service
 public class LocationService {
 
